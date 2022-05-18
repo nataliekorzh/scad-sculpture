b71376e289806d7f73f441f804b396adb23f6813
nataliekorzh <nkorzh@oberlin.edu>
2022-05-18 12:12:37 -0400

    Adding files for pixel and sculpture generation

---
+++ circle-mosaic.c
@@ -0,0 +1,163 @@
+/*
+   Code for rendering a target image as a collection of black squares
+   of varying size.  Written by R. Bosch in 2018. Feel free to modify.
+
+   The target image is described in the file
+
+     image.pgm
+
+   and the final artwork is stored in the file
+
+     square_mosaic.eps
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define ROWS_P 100               /* # of rows of pixels in the target image */
+#define COLS_P 100                /* # of columns of pixels in the target image */
+#define K 1                      /* The pixels are grouped in KxK blocks */
+
+/* Note: ROWS_P and COLS_P should be divisible by K */
+
+#define ROWS_M ROWS_P/K            /* # of rows of squares in the mosaic */
+#define COLS_M COLS_P/K            /* # of columns of squares in the mosaic */
+#define BW 2                       /* border width (measured in squares) */
+
+#define LW 0.10                    /* linewidth of the lines in the mosaic */
+#define SW 10.00                   /* width of the squares in the mosaic */
+
+#define RBACK 1.0                  /* RGB values for the background */
+#define GBACK 1.0
+#define BBACK 1.0
+#define RLINE 0.0                  /* RGB values for the lines */
+#define GLINE 0.0
+#define BLINE 0.0
+
+int brightness[ROWS_P][COLS_P];    /* brightness of pixel (i,j) on a */
+                                   /*  0-to-255 black-to-white scale */
+double b[ROWS_M][COLS_M];          /* avg. brightness of square (i,j) on */
+                                   /*  a 0.0-to-1.0 black-to-white scale */
+double x, y, sw;
+
+int main ()
+{
+int i, j, ip, jp, trash_int; 
+     
+FILE *image, *eps;
+     
+char trash_string[25];
+     
+/* 
+   Read the PGM file that describes the target image.
+ */
+
+if ((image=fopen("eye.pgm", "r")) == NULL) 
+  {
+    printf("Couldn't open cat.pgm!\n");
+    exit(0);
+  }
+
+fscanf(image, "%s", trash_string);
+
+fscanf(image, "%d", &trash_int);
+fscanf(image, "%d", &trash_int);
+fscanf(image, "%d", &trash_int);
+
+for (i = 0; i < ROWS_P; i++)
+  for (j = 0; j < COLS_P; j++)
+    fscanf(image, "%d", &brightness[i][j]);
+
+fclose(image);
+
+/* 
+   Use the pixel brightness values (on a 0-to-255 black-to-white scale) 
+   to compute block brightness values (on a 0.0-to-1.0 black-to-white scale). 
+ */
+
+for (i = 0; i < ROWS_M; i++)
+  for (j = 0; j < COLS_M; j++)
+    {
+      b[i][j] = 0.0;
+      for (ip = 0; ip < K; ip++)
+        for (jp = 0; jp < K; jp++)
+          b[i][j] = b[i][j] + brightness[i*K+ip][j*K+jp];
+      b[i][j] = b[i][j]/(K*K);
+      b[i][j] = b[i][j]/255.0;
+      if (b[i][j] < 0.0)
+        b[i][j] = 0.0;
+      if (b[i][j] > 1.0)
+        b[i][j] = 1.0;
+    }
+
+/*
+   Open the PostScript file.  Assign values to setlinecap, setlinejoin, and 
+   setlinewidth.
+ */
+
+if ((eps=fopen("circle_mosaic.eps", "w")) == NULL)
+  {
+    printf("Couldn't open cat.eps!\n");
+    exit(0);
+  }
+
+fprintf(eps, "%%!PS-Adobe-3.0 EPSF-3.0\n");
+fprintf(eps, "%%%%BoundingBox: 0 0 %d %d\n\n", 
+  (int) ((COLS_M+2*BW)*SW), (int) ((ROWS_M+2*BW)*SW));
+
+fprintf(eps, "1 setlinecap\n");
+fprintf(eps, "1 setlinejoin\n");
+fprintf(eps, "%.3f setlinewidth\n\n", LW);
+
+/* Construct the PostScript code for drawing a square. */
+
+fprintf(eps, "/square { 3 dict begin\n");
+fprintf(eps, "  /sw exch def\n");
+fprintf(eps, "  /y exch def\n");
+fprintf(eps, "  /x exch def\n");
+fprintf(eps, "  x y translate\n");
+fprintf(eps, "  /r sw 0.5 mul def\n");
+fprintf(eps, "  0 0 r 0 360 arc\n");
+fprintf(eps, "  fill\n");
+fprintf(eps, "  x neg y neg translate\n");
+fprintf(eps, "end } def\n\n");
+
+/* Draw background */
+
+fprintf(eps, "%.3f %.3f %.3f setrgbcolor\n\n", RBACK, GBACK, BBACK);
+  
+fprintf(eps, "newpath\n");
+fprintf(eps, "%.6f %.6f moveto\n", 0.0, 0.0);
+fprintf(eps, "%.6f %.6f lineto\n", 0.0, (ROWS_M+2*BW)*SW);
+fprintf(eps, "%.6f %.6f lineto\n", (COLS_M+2*BW)*SW, (ROWS_M+2*BW)*SW);
+fprintf(eps, "%.6f %.6f lineto\n", (COLS_M+2*BW)*SW, 0.0);
+fprintf(eps, "closepath\n");
+fprintf(eps, "fill\n\n");
+
+/*  
+   Draw the squares. Square (i,j) is centered at 
+
+       (x,y) = (SW*BW + SW*0.5 + SW*j, SW*BW + SW*0.5 + SW*(ROWS_M-1-i)).  
+ */
+
+fprintf(eps, "%.3f %.3f %.3f setrgbcolor\n\n", RLINE, GLINE, BLINE);
+
+for (i = 0; i < ROWS_M; i++)
+  for (j = 0; j < COLS_M; j++)
+    {
+      x = SW*BW + SW*0.5 + SW*j;
+      y = SW*BW + SW*0.5 + SW*(ROWS_M-1-i);
+      sw = SW*(0.10+0.80*(1.0 - b[i][j]));
+      if (b[i][j] == 0) 
+        {
+          fprintf(eps, "%.3f %.3f %.3f square\n", x, y, sw);
+        }
+    }
+
+fprintf(eps, "showpage\n");                                                
+fprintf(eps, "%%EOF\n");
+
+fclose(eps);
+
+}
---
+++ sculpture.py
@@ -0,0 +1,88 @@
+import sys
+import random
+
+def parse_image_OneSided(fileName):
+    rows = 0
+    cols = 0
+    image = []
+    first = []
+    columns = []
+    test = []
+    with open(fileName, 'r') as file:
+        file.readline()
+        dimensions = file.readline().strip().split()
+        rows = int(dimensions[0])
+        cols = int(dimensions[1])
+        for i in range(rows):
+            for j in range(cols):
+                pixel = int(file.readline().strip())
+                if pixel == 0:
+                    z = random.randint(0,100) #create random z coordinate
+                    image.append([j,z,-i]) #place at row,col,z (rotated 90 degrees so its upright)
+    return image
+
+def parse_image_Wrong(fileName):
+    rows = 0
+    cols = 0
+    image = []
+    first = []
+    columns = []
+    test = []
+    with open(fileName, 'r') as file:
+        file.readline()
+        dimensions = file.readline().strip().split()
+        rows = int(dimensions[0])
+        cols = int(dimensions[1])
+        for i in range(rows):
+            for j in range(cols):
+                pixel = int(file.readline().strip())
+                if pixel == 0:
+                    z = random.randint(0,100)
+                    first.append([j,0,-i]) #grab columns and rows for image as in 1 sided version
+                    test.append([j,0,-i])
+                    columns.append(j)
+    image.append(first)
+    print(test)
+    extra = []
+    columns = set(columns)
+    columns = list(columns)
+    for col in columns:
+        row = []
+        print(col)
+        for point in first: #fixes weird python object issues
+            new_point = [0,0,0]
+            new_point[0] = point[0]
+            new_point[1] = col
+            new_point[2] = point[2]
+            row.append(new_point)
+        image.append(row)
+    
+    random.shuffle(columns)
+    counter = 0
+    for col in columns:
+        test[counter][1] = col #this loses the image entirely. have to rethink combinatorics
+        counter += 1
+    return image, test
+
+def main():
+    fileName = 'coordinates.txt'
+    imageFile = sys.argv[1]
+
+    image, test = parse_image(imageFile)
+    
+    with open(fileName, 'w') as file:
+        num = 'i'
+        for row in image:
+            file.write(num + "vertices = scale*[\n")
+            for i in row:
+                file.write("[" + str(i[0]) + "," + str(i[1]) + "," + str(i[2]) + "],\n")
+            file.write("];\n")
+            num += 'i'
+    
+    with open('test.txt', 'w') as file:
+        file.write("test = scale*[\n")
+        for row in test:
+            file.write("[" + str(row[0]) + "," + str(row[1]) + "," + str(row[2]) + "],\n")
+        file.write("\;\n")
+
+main()
