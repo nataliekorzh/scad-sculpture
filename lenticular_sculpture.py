26188c270d905bd7660b46937402af86b4a60a89
nataliekorzh <nkorzh@oberlin.edu>
2022-05-31 16:06:41 -0400

    scripts create scad files and lenticular art in one dim!

---
+++ lenticular_sculpture.py
@@ -0,0 +1,80 @@
+import sys
+import random
+
+def lenticulate(image, uc):
+    final = []
+    uc = list(uc)
+    num = int(len(uc)/5)
+    chunks=[uc[i:i + num] for i in range(0, len(uc), num)]
+    groups = {}
+    for index in range(len(chunks)):
+        for c in chunks[index]:
+            groups[c] = index
+    print(groups)
+    counter = 10
+    mv = 0
+    for row in image:
+        for col in image[row]:
+            final.append([col,counter*groups[col],row])
+            #if counter % group == 0:
+            #    counter = 1
+            #    mv += 1
+            #else:
+            #    counter += 1 
+    return final
+
+def parse_image(fileName):
+    rows = 0
+    cols = 0
+    image = {}
+    first = []
+    columns = []
+    test = []
+    maxcol = 0
+    mincol = 100
+    unique_col = set()
+    with open(fileName, 'r') as file:
+        file.readline()
+        dimensions = file.readline().strip().split()
+        rows = int(dimensions[0])
+        cols = int(dimensions[1])
+        for i in range(rows):
+            for j in range(cols):
+                pixel = int(file.readline().strip())
+                if pixel == 0:
+                    z = random.randint(0,100) #create random z coordinate
+                    if -i not in image:
+                        image[-i] = []
+                    image[-i].append(j)
+                    unique_col.add(j)
+                    if j < mincol:
+                        mincol = j
+                    if j > maxcol:
+                        maxcol = j
+    print(len(unique_col))
+    return lenticulate(image, unique_col)
+
+def main():
+    fileName = 'lenticular_eye.scad'
+    imageFile = sys.argv[1]
+
+    image = parse_image(imageFile)
+    
+    with open(fileName, 'w') as file:
+        file.write('point_radius = 5; // radius of a point (a sphere)\n')
+        file.write('scale = 10;   // a scaling factor (increase to expand, decrease to contract\n')
+        file.write('// module for drawing a point (a sphere)\n\n')
+        file.write('module point(v) {\n')
+        file.write('translate(v) sphere(r=point_radius);\n')
+        file.write('}\n\n')
+
+        file.write("vertices = scale*[\n")
+        for row in image:
+            file.write("[" + str(row[0]) + "," + str(row[1]) + "," + str(row[2]) + "],\n")
+        file.write("];\n\n\n")
+        file.write('color([0,0,0])\n')
+        file.write('// Draw one point (a sphere) for each vertex\n')
+        file.write('for (i=[0:len(vertices)-1])\n')
+        file.write('  point(vertices[i]);\n')
+
+main()
--- sculpture.py
+++ sculpture.py
@@ -1,88 +1,45 @@
 import sys
 import random
 
-def parse_image_OneSided(fileName):
+def parse_image(fileName):
     rows = 0
     cols = 0
     image = []
-    first = []
-    columns = []
-    test = []
     with open(fileName, 'r') as file:
         file.readline()
-        dimensions = file.readline().strip().split()
+        dimensions = file.readline().strip().split() # get dimensions
         rows = int(dimensions[0])
         cols = int(dimensions[1])
         for i in range(rows):
-            for j in range(cols):
+            for j in range(cols): #loop through rows and columns using dims
                 pixel = int(file.readline().strip())
-                if pixel == 0:
+                if pixel == 0: #if black (vs white) pixel
                     z = random.randint(0,100) #create random z coordinate
                     image.append([j,z,-i]) #place at row,col,z (rotated 90 degrees so its upright)
     return image
 
-def parse_image_Wrong(fileName):
-    rows = 0
-    cols = 0
-    image = []
-    first = []
-    columns = []
-    test = []
-    with open(fileName, 'r') as file:
-        file.readline()
-        dimensions = file.readline().strip().split()
-        rows = int(dimensions[0])
-        cols = int(dimensions[1])
-        for i in range(rows):
-            for j in range(cols):
-                pixel = int(file.readline().strip())
-                if pixel == 0:
-                    z = random.randint(0,100)
-                    first.append([j,0,-i]) #grab columns and rows for image as in 1 sided version
-                    test.append([j,0,-i])
-                    columns.append(j)
-    image.append(first)
-    print(test)
-    extra = []
-    columns = set(columns)
-    columns = list(columns)
-    for col in columns:
-        row = []
-        print(col)
-        for point in first: #fixes weird python object issues
-            new_point = [0,0,0]
-            new_point[0] = point[0]
-            new_point[1] = col
-            new_point[2] = point[2]
-            row.append(new_point)
-        image.append(row)
-    
-    random.shuffle(columns)
-    counter = 0
-    for col in columns:
-        test[counter][1] = col #this loses the image entirely. have to rethink combinatorics
-        counter += 1
-    return image, test
-
 def main():
-    fileName = 'coordinates.txt'
+    fileName = 'squirrel_random.scad'
     imageFile = sys.argv[1]
 
-    image = parse_image_OneSided(imageFile)
-    """
+    image = parse_image(imageFile)
+    
     with open(fileName, 'w') as file:
-        num = 'i'
+        file.write('point_radius = 5; // radius of a point (a sphere)\n')
+        file.write('scale = 10;   // a scaling factor (increase to expand, decrease to contract\n')
+        file.write('// module for drawing a point (a sphere)\n\n')
+        file.write('module point(v) {\n')
+        file.write('translate(v) sphere(r=point_radius);\n')
+        file.write('}\n\n')
+
+        file.write("vertices = scale*[\n")
         for row in image:
-            file.write(num + "vertices = scale*[\n")
-            for i in row:
-                file.write("[" + str(i[0]) + "," + str(i[1]) + "," + str(i[2]) + "],\n")
-            file.write("];\n")
-            num += 'i'
-    """
-    with open('squirrel_random.txt', 'w') as file:
-        file.write("test = scale*[\n")
-        for row in image:
-            file.write("[" + str(row[0]) + "," + str(row[1]) + "," + str(row[2]) + "],\n")
-        file.write("];\n")
+            file.write("[" + str(row[0]) + "," + str(row[1]) + "," + str(row[2]) + "],\n") # add each vertex
+        
+        file.write("];\n\n\n")
+        file.write('color([0,0,0])\n')
+        file.write('// Draw one point (a sphere) for each vertex\n')
+        file.write('for (i=[0:len(vertices)-1])\n')
+        file.write('  point(vertices[i]);\n')
 
 main()
